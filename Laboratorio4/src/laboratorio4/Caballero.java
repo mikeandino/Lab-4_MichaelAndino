2a09d12738e1b158f5d1077ebe9b3ae48a830ad0
Michael <mikeandinor98@gmail.com>
2017-08-11 16:39:17 -0600

    Excepcion

--- Laboratorio4/src/laboratorio4/Caballero.java
+++ Laboratorio4/src/laboratorio4/Caballero.java
@@ -14,8 +14,37 @@ public class Caballero extends Pieza {
 
     @Override
     public Pieza[][] movimiento(Pieza[][] tablero) throws FueraDelTablero {
+        try {
+            System.out.println("Puedes moverte: ");
+            try {
+                if (tablero[x][y-1]==null) {
+                    System.out.println("1.Arriba");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x-1][y]==null) {
+                    System.out.println("2.Izquierda");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x-1][y]==null) {
+                    System.out.println("3.Derecha");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x-1][y]==null) {
+                    System.out.println("4.Abajo");
+                }
+            } catch (Exception e) {
+            }
+            System.out.print("Ingrese el numero donde desea m");
+        } catch (FueraDelTablero e) {
+            System.out.println(e.getMessage());
+        }
         return tablero;
-
     }
 
     @Override
