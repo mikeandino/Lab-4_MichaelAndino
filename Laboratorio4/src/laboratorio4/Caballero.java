7c405618b6e04da47416c308c2c2b6c2d95645a2
Michael <mikeandinor98@gmail.com>
2017-08-11 17:08:52 -0600

    Pull

--- Laboratorio4/src/laboratorio4/Caballero.java
+++ Laboratorio4/src/laboratorio4/Caballero.java
@@ -18,55 +18,91 @@ public class Caballero extends Pieza {
         try {
             System.out.println("Puedes moverte: ");
             try {
-                if (tablero[x-1][y]==null) {
+                if (tablero[x - 1][y] == null || tablero[x - 1][y].color != tablero[x][y].color) {
                     System.out.println("1.Arriba");
                 }
             } catch (Exception e) {
             }
             try {
-                if (tablero[x][y-1]==null) {
-                    System.out.println("2.Izquierda");
+                if (tablero[x - 1][y - 1].color != tablero[x][y].color) {
+                    System.out.println("2.Arriba-Izquierda");
                 }
             } catch (Exception e) {
             }
             try {
-                if (tablero[x][y+1]==null) {
-                    System.out.println("3.Derecha");
+                if (tablero[x][y - 1] == null || tablero[x][y - 1].color != tablero[x][y].color) {
+                    System.out.println("3.Izquierda");
                 }
             } catch (Exception e) {
             }
             try {
-                if (tablero[x+1][y]==null) {
-                    System.out.println("4.Abajo");
+                if (tablero[x + 1][y - 1] == null || tablero[x][y - 1].color != tablero[x][y].color) {
+                    System.out.println("4.Abajo-Izquierda");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x - 1][y + 1].color != tablero[x][y].color) {
+                    System.out.println("5.Arriba-Derecha");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x][y + 1] == null || tablero[x - 1][y].color != tablero[x][y].color) {
+                    System.out.println("6.Derecha");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x][y + 1] == null || tablero[x - 1][y].color != tablero[x][y].color) {
+                    System.out.println("7.Abajo-Derecha");
+                }
+            } catch (Exception e) {
+            }
+            try {
+                if (tablero[x + 1][y] == null || tablero[x - 1][y].color != tablero[x][y].color) {
+                    System.out.println("8.Abajo");
                 }
             } catch (Exception e) {
             }
             System.out.print("Ingrese el numero donde desea moverse: ");
             int opcion = new Scanner(System.in).nextInt();
-            while (opcion<1||opcion>4) {
+            while (opcion < 1 || opcion > 8) {
                 System.out.print("Numero Invalido: ");
                 opcion = new Scanner(System.in).nextInt();
             }
             switch (opcion) {
                 case 1:
-                    tablero[x-1][y]=tablero[x][y];
-                    tablero[x][y]=null;
-                    tablero[x-1][y].setX(x-1);
-                    break;
-                case 2:
-                    tablero[x][y-1]=tablero[x][y];
-                    tablero[x][y]=null;
-                    tablero[x][y-1].setY(y-1);
+                    try {
+                        if (tablero[x - 1][y].equals(null)) {
+                            tablero[x - 1][y] = tablero[x][y];
+                            tablero[x][y] = null;
+                            tablero[x - 1][y].setX(x - 1);
+                        } else {
+                            tablero=comer(tablero);
+                        }
+                    } catch (Exception e) {
+
+                    }
                     break;
                 case 3:
-                    tablero[x][y+1]=tablero[x][y];
-                    tablero[x][y]=null;
-                    tablero[x][y+1].setY(y+1);
+                    tablero[x][y - 1] = tablero[x][y];
+                    tablero[x][y] = null;
+                    tablero[x][y - 1].setY(y - 1);
                     break;
-                case 4:
-                    tablero[x-1][y]=tablero[x][y];
-                    tablero[x][y]=null;
-                    tablero[x-1][y].setX(x-1);
+                case 6:
+                    tablero[x][y + 1] = tablero[x][y];
+                    tablero[x][y] = null;
+                    tablero[x][y + 1].setY(y + 1);
+                    break;
+                case 8:
+                    tablero[x - 1][y] = tablero[x][y];
+                    tablero[x][y] = null;
+                    tablero[x - 1][y].setX(x - 1);
+                    break;
+                default:
+                    System.out.println("Opcion Invalida");
+                    opcion = new Scanner(System.in).nextInt();
                     break;
             }
         } catch (FueraDelTablero e) {
